b88222803d6a2efd986353651221bdc0f515b1aa
Bhavesh-Codesis <bhavesh@codesis.tech>
2024-07-19 18:24:56 +0530

    Read Description
    1. created tabination
    2. working on account metrics page

---
+++ src/pages/AccountMetrics/Charts/BalanceChart.jsx
@@ -0,0 +1,137 @@
+import React, {useState, useEffect} from "react";
+import ReactApexChart from "react-apexcharts";
+import {utc_to_eet} from "../../../utils/helpers/string";
+// BalanceData;
+const BalanceChart = ({PerformanceChart}) => {
+  const dummyData = [
+    {
+      balance: 100000,
+      equity: 100000,
+      time: "2024-07-19 06:27:00.609012",
+    },
+  ];
+
+  const [series, setSeries] = useState([
+    {
+      name: "Equity",
+      type: "line",
+      data: [103, 20, 50, 30],
+    },
+    {
+      name: "Balance",
+      type: "line",
+      data: [13, 245, 2, 52],
+    },
+  ]);
+  const [options, setOptions] = useState({
+    legend: {
+      fontSize: "14px",
+      fontWeight: 700,
+      labels: {
+        colors: "white",
+      },
+      markers: {
+        width: 15,
+        height: 15,
+      },
+    },
+    colors: ["#FF9800", "#FD504F"],
+    chart: {
+      fontFamily: "Outfit, sans-serif",
+      toolbar: {
+        show: false,
+      },
+      height: 350,
+      type: "line",
+      zoom: {
+        enabled: false,
+      },
+      animations: {
+        enabled: false,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: "straight",
+    },
+    grid: {
+      borderColor: "#415672",
+      strokeDashArray: 8,
+    },
+    labels: (dummyData && Object.keys(dummyData).map((data) => utc_to_eet(dummyData[data].time))) || [],
+    xaxis: {
+      type: "datetime",
+      tooltip: {
+        enabled: false,
+      },
+      axisTicks: {
+        show: false,
+      },
+      axisBorder: {
+        show: false,
+      },
+      labels: {
+        show: true,
+        style: {
+          colors: "#8B8E93",
+          fontSize: "12px",
+        },
+      },
+    },
+    yaxis: {
+      type: "numeric",
+      labels: {
+        style: {
+          colors: "#8B8E93",
+          fontSize: "12px",
+        },
+      },
+    },
+    tooltip: {
+      style: {
+        fontSize: "14px",
+        fontWeight: "700",
+      },
+      x: {
+        show: true,
+        formatter: (val) => {
+          const date = new Date(val);
+          return `${date.toDateString()} ${date.toLocaleTimeString()}`;
+        },
+      },
+    },
+  });
+
+  useEffect(() => {
+    let balancedata = dummyData && Object.keys(dummyData).map((data) => dummyData[data].balance);
+    let equitydata = dummyData && Object.keys(dummyData).map((data) => dummyData[data].equity);
+
+    setOptions({
+      ...options,
+      labels: (dummyData && Object.keys(dummyData).map((data) => utc_to_eet(dummyData[data].time))) || [],
+    });
+    setSeries([
+      {
+        name: "Equity",
+        data: equitydata || [],
+      },
+      {
+        name: "Balance",
+        data: balancedata || [],
+      },
+    ]);
+  }, [dummyData]);
+
+  return (
+    <div id="chart">
+      <ReactApexChart
+        options={options}
+        series={series}
+        height={250}
+      />
+    </div>
+  );
+};
+export default BalanceChart;
---
+++ src/pages/AccountMetrics/Charts/ProfitChart.jsx
@@ -0,0 +1,184 @@
+import React, {useState, useEffect} from "react";
+import ReactApexChart from "react-apexcharts";
+import {dollarUS, formatDate, utc_to_eet} from "../../../utils/helpers/string";
+
+const ProfitChart = ({data, ProfitData}) => {
+  const [Series, setSeries] = useState([
+    {
+      name: "Balance",
+      type: "line",
+      data: [],
+    },
+  ]);
+  const [Options, setOptions] = useState({
+    plotOptions: {
+      bar: {
+        borderRadius: 5,
+      },
+    },
+    fill: {
+      opacity: [1, 0.5],
+      type: ["solid"],
+      gradient: {
+        type: "vertical",
+        shadeIntensity: 0,
+        gradientToColors: undefined,
+        opacityFrom: 0.8,
+        opacityTo: 0.2,
+        stops: [0, 90, 100],
+      },
+    },
+
+    legend: {
+      fontSize: "14px",
+      fontWeight: 700,
+      labels: {
+        colors: "white",
+      },
+      markers: {
+        width: 15,
+        height: 15,
+      },
+    },
+    colors: ["#FD504F", "#FF9800"],
+    chart: {
+      fontFamily: "Outfit, sans-serif",
+      toolbar: {
+        show: false,
+      },
+      height: 350,
+      type: "line",
+      zoom: {
+        enabled: true,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      width: [4, 0],
+      curve: "smooth",
+    },
+    grid: {
+      borderColor: "#415672",
+      strokeDashArray: 8,
+    },
+    labels:
+      (ProfitData &&
+        ProfitData.date &&
+        ProfitData.date.map((date_str) => {
+          return new Date(new Date(date_str).toLocaleString("en-US", {timeZone: "EET"}));
+        })) ||
+      [],
+    xaxis: {
+      categories:
+        (ProfitData &&
+          ProfitData.date &&
+          ProfitData.date.map((date_str) => {
+            return new Date(new Date(date_str).toLocaleString("en-US", {timeZone: "EET"}));
+          })) ||
+        [],
+      tooltip: {
+        enabled: false,
+      },
+      axisTicks: {
+        show: false,
+      },
+      axisBorder: {
+        show: false,
+      },
+      labels: {
+        formatter: (value) => {
+          return formatDate(value);
+        },
+        style: {
+          colors: "#8B8E93",
+          fontSize: "12px",
+        },
+      },
+    },
+    yaxis: {
+      axisTicks: {
+        show: false,
+      },
+      axisBorder: {
+        show: false,
+      },
+      // tickAmount: 16,
+      labels: {
+        formatter: (value) => {
+          return dollarUS(value);
+        },
+        style: {
+          colors: "#8B8E93",
+          fontSize: "12px",
+        },
+      },
+    },
+
+    tooltip: {
+      style: {
+        fontSize: "14px",
+        fontWeight: "700",
+      },
+      custom: function ({series, seriesIndex, dataPointIndex, w}) {
+        const date = new Date(w.config.Options.labels[dataPointIndex]);
+        return (
+          '<div class="arrow_box">' +
+          '<div class="Annotation-box Red">' +
+          "<div>" +
+          " Profit : " +
+          "</div>" +
+          "<span>" +
+          series[seriesIndex][dataPointIndex] +
+          "</span>" +
+          "</div>" +
+          '<div class="Annotation-box Date">' +
+          "<div>" +
+          "</div>" +
+          "<span>" +
+          `${date.toDateString()} ${date.toLocaleTimeString()}` +
+          "</span>" +
+          "</div>" +
+          "</div>"
+        );
+      },
+    },
+  });
+
+  useEffect(() => {
+    const profit = ProfitData && Object.keys(ProfitData.profit).map((data) => ProfitData.profit[data]);
+
+    if (ProfitData) {
+      setSeries([
+        {
+          name: "Balance",
+          type: "line",
+          data: profit,
+        },
+      ]);
+
+      setOptions({
+        Options,
+        labels:
+          (ProfitData &&
+            ProfitData.date &&
+            ProfitData.date.map((date_str) => {
+              return new Date(new Date(date_str).toLocaleString("en-US", {timeZone: "EET"}));
+            })) ||
+          [],
+      });
+    }
+  }, [ProfitData]);
+
+  return (
+    <div id="chart">
+      <ReactApexChart
+        options={Options}
+        series={Series}
+        height={250}
+      />
+    </div>
+  );
+};
+export default ProfitChart;
