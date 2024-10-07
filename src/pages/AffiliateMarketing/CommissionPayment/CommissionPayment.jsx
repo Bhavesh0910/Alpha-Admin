f42bfe1df1a983258bde3e810362c1e8eb182254
codesis-aakriti <aakriti@codesis.tech>
2024-10-07 15:16:59 +0530

    add pagination in commissions

--- src/pages/AffiliateMarketing/CommissionPayment/CommissionPayment.jsx
+++ src/pages/AffiliateMarketing/CommissionPayment/CommissionPayment.jsx
@@ -9,17 +9,16 @@ import dayjs from "dayjs";
 
 const CommissionPayment = ({ id }) => {
   const [searchText, setSearchText] = useState("");
-  const [pageSize, setPageSize] = useState(20);
+  const [pageSize, setPageSize] = useState(10); 
   const [pageNo, setPageNo] = useState(1);
   const dispatch = useDispatch();
-  const { commissionPaymentData, codes ,  isLoading, totalCount } = useSelector((state) => state.affiliate);
+  const { commissionPaymentData, count , isLoading , totalItems } = useSelector((state) => state.affiliate);
   const { idToken } = useSelector((state) => state.auth);
 
+  console.log(commissionPaymentData)
   useEffect(() => {
     dispatch(fetchCommissionPaymentData({ idToken, affiliateId: id, pageNo, pageSize, searchText }));
   }, [dispatch, pageNo, pageSize, searchText, idToken, id]);
-
-  
 
   const columns = [
     {
@@ -27,7 +26,7 @@ const CommissionPayment = ({ id }) => {
       dataIndex: "created_at",
       key: "created_at",
       render: (text) => (text ? dayjs(text).format('DD/MMM/YYYY') : "-"),
-      width:100
+      width: 100
     },
     {
       title: "Email",
@@ -55,17 +54,17 @@ const CommissionPayment = ({ id }) => {
       render: (text) => text || "-",
     },
     {
-        title: "Referred Trader",
-        dataIndex: "refered_trader",
-        key: "refered_trader",
-        render: (text) => (text !== null ? text : "-"),
-      },
-      {
-        title: "Payment ID",
-        dataIndex: ["payment", "payment_id"],
-        key: "payment_id",
-        render: (text) => text || "-",
-      },
+      title: "Referred Trader",
+      dataIndex: "refered_trader",
+      key: "refered_trader",
+      render: (text) => (text !== null ? text : "-"),
+    },
+    {
+      title: "Payment ID",
+      dataIndex: ["payment", "payment_id"],
+      key: "payment_id",
+      render: (text) => text || "-",
+    },
   ];
 
   const handleSearch = (value) => {
@@ -73,6 +72,10 @@ const CommissionPayment = ({ id }) => {
     setPageNo(1); // Reset to the first page on search
   };
 
+  function triggerChange(page, updatedPageSize) {
+    setPageNo(page);
+    setPageSize(updatedPageSize);
+  }
   return (
     <div className="commission_payment">
       <div className="header_wrapper">
@@ -90,14 +93,14 @@ const CommissionPayment = ({ id }) => {
         <LoaderOverlay />
       ) : (
         <AntTable
-          data={commissionPaymentData || []}
+          data={commissionPaymentData?.results || []}
           columns={columns}
-          totalPages={Math.ceil(commissionPaymentData?.length / pageSize)}
-          totalItems={commissionPaymentData?.length}
+          totalPages={Math.ceil(commissionPaymentData?.count / pageSize)} 
+          totalItems={commissionPaymentData?.count || 0} 
           pageSize={pageSize}
           CurrentPageNo={pageNo}
           setPageSize={setPageSize}
-          triggerChange={setPageNo}
+          triggerChange={triggerChange}
           scrollY={400}
         />
       )}
