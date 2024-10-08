0910e8ffd6a3b89c38a8be5fee55729258b2647a
Kajal-Codesis <kajal@codesis.tech>
2024-10-08 13:07:20 +0530

    read desc
    - Created competition leaderboard page UI.
    - Worked on the responsiveness of the same.
    - Integrated the competition leaderboard table, chart and stats api.

---
+++ src/components/Competition/Cards/index.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import "./style.scss";
+import compicon from "../../../assets/images/competition.svg";
+import fundingicon from "../../../assets/images/fundingIcon.svg";
+import lossicon from "../../../assets/images/lossIcon.svg";
+import winicon from "../../../assets/images/winIcon.svg";
+import winnericon from "../../../assets/images/currentWinnerIcon.svg";
+import total from "../../../assets/images/total.svg";
+import {useSelector} from "react-redux";
+const Cards = () => {
+  const {compDashboard, compTableData} = useSelector((state) => state.comp);
+//   console.log("compDashboard", compDashboard);
+  return (
+    <div className="Cards_wrapper">
+      <div className="Cards_content">
+        <div className="Cards_content_wrapper">
+          <img
+            className="Cards_icon"
+            src={compicon}
+          />
+          <p className="Cards_label">Equity</p>
+        </div>
+
+        <p className="Cards_value">{compDashboard?.user?.balance_equity?.equity && compDashboard?.user?.balance_equity?.equity}</p>
+      </div>
+      <div className="Cards_content">
+        <div className="Cards_content_wrapper">
+          <img
+            className="Cards_icon"
+            src={fundingicon}
+          />
+          <p className="Cards_label">Balance</p>
+        </div>
+        <p className="Cards_value">{compDashboard?.user?.balance_equity?.balance && compDashboard?.user?.balance_equity?.balance}</p>
+      </div>
+      <div className="Cards_content">
+        <div className="Cards_content_wrapper">
+          <img
+            className="Cards_icon"
+            src={lossicon}
+          />
+          <p className="Cards_label">P&L</p>
+        </div>
+        <p className="Cards_value">{compDashboard?.user?.total_returns && compDashboard?.user?.total_returns.toFixed(2)}</p>
+      </div>
+      <div className="Cards_content">
+        <div className="Cards_content_wrapper">
+          <img
+            className="Cards_icon"
+            src={winicon}
+          />
+          <p className="Cards_label">Win Rate</p>
+        </div>
+        <p className="Cards_value">{compDashboard?.user?.win_rate && (compDashboard?.user?.win_rate).toFixed(2)}</p>
+      </div>
+      <div className="Cards_content">
+        <div className="Cards_content_wrapper">
+          <img
+            className="Cards_icon"
+            src={winnericon}
+          />
+          <p className="Cards_label">Current Winner</p>
+        </div>
+        <p className="Cards_value">{compDashboard?.user?.name && compDashboard?.user?.name || compTableData && compTableData[0]?.name}</p>
+      </div>
+      <div className="Cards_content">
+        <div className="Cards_content_wrapper">
+          <img
+            className="Cards_icon"
+            src={total}
+          />
+          <p className="Cards_label">Total Contestants</p>
+        </div>
+        <p className="Cards_value">{compDashboard?.user?.total_contestants && compDashboard?.user?.total_contestants}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
---
+++ src/components/Competition/CurrentWinner/index.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./style.scss";
+import { useSelector } from "react-redux";
+const CurrentWinner = () => {
+    const {compDashboard}=useSelector((state)=>state.comp);
+    return (
+    <div className="current_winner_wrapper">
+      <h2>Dashboard Stats</h2>
+      <div className="current_winner_groups">
+        <div className="current_winner_info_groupA">
+          <div className="current_winner_info">
+            <div className="title">Current Equity</div>
+            <p>{compDashboard?.user?.balance_equity?.equity && compDashboard?.user?.balance_equity?.equity}</p>
+          </div>
+          <div className="current_winner_info">
+            <div className="title">Starting Balance</div>
+            <p>{compDashboard?.user?.account_size && compDashboard?.user?.account_size}</p>
+          </div>
+          <div className="current_winner_info">
+            <div className="title">% Return</div>
+            <p>{compDashboard?.user?.return_percentage && (compDashboard?.user?.return_percentage.toFixed(2))}</p>
+          </div>
+          <div className="current_winner_info">
+            <div className="title">Most traded product</div>
+            <p>{compDashboard?.user?.most_traded_product && compDashboard?.user?.most_traded_product}</p>
+          </div>
+          <div className="current_winner_info">
+            <div className="title">Daily loss</div>
+            <p>{compDashboard?.user?.max_daily_loss && compDashboard?.user?.max_daily_loss}</p>
+          </div>
+        </div>
+        <div className="current_winner_info_groupB">
+          <div className="current_winner_info">
+            <div className="title">Current Balance</div>
+            <p>{compDashboard?.user?.balance_equity?.balance && compDashboard?.user?.balance_equity?.balance}</p>
+          </div>
+          <div className="current_winner_info">
+            <div className="title">Cummulative PnL</div>
+            <p>{compDashboard?.user?.total_returns && compDashboard?.user?.total_returns}</p>
+          </div>
+          <div className="current_winner_info">
+            <div className="title">Largest Winning Trade</div>
+            <p>{compDashboard?.user?.largest_winning_trade && compDashboard?.user?.largest_winning_trade}</p>
+          </div>
+          <div className="current_winner_info">
+            <div className="title">Max Loss</div>
+            <p>{compDashboard?.user?.max_loss && compDashboard?.user?.max_loss}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CurrentWinner;
